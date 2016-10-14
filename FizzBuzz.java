56bc65143f567edbf468429833dd37600d8f198f
szgabbor <szgabbor@gmail.com>
2016-10-14 16:34:30 +0200

    fizzBuzz funciton added

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -7,13 +7,15 @@ public class FizzBuzz {
   
   public static void run() {
     for (int i = 0; i < 100; i++) {
-      System.out.println(fizz(i));
+      System.out.println(fizzBuzz(i));
     }
   }
 
-  public static String fizz(int number) {
-    String result = "" + number;
-    if (number % 5 == 0) result = "Fizz";
+  public static String fizzBuzz(int number) {
+    String result = "";
+    if (number % 5 == 0) result += "Fizz";
+    if (number % 7 == 0) result += "Buzz";
+    if (result.equals("")) result = "" + number;
     return result;
   }
 
