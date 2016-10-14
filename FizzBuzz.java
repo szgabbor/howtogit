ebd7d78d339b0534e7171c9173237499b46e8a12
szgabbor <szgabbor@gmail.com>
2016-10-14 16:48:05 +0200

    we can determine the steps

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,19 +3,21 @@ public class FizzBuzz {
 
   public static void main(String[] args) {
     int maxNumber = Integer.parseInt(args[0]);
-    run(maxNumber);
+    int step1 = Integer.parseInt(args[1]);
+    int step2 = Integer.parseInt(args[2]);
+    run(maxNumber, step1, step2);
   }
   
-  public static void run(int maxNumber) {
+  public static void run(int maxNumber, int step1, int step2) {
     for (int i = 0; i < maxNumber; i++) {
-      System.out.println(fizzBuzz(i));
+      System.out.println(fizzBuzz(i, step1, step2));
     }
   }
 
-  public static String fizzBuzz(int number) {
+  public static String fizzBuzz(int number, int step1, int step2) {
     String result = "";
-    if (number % 5 == 0) result += "Fizz";
-    if (number % 7 == 0) result += "Buzz";
+    if (number % step1 == 0) result += "Fizz";
+    if (number % step2 == 0) result += "Buzz";
     if (result.equals("")) result = "" + number;
     return result;
   }
