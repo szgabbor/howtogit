99776b86805e23e5f1872b91f8a72f67d99a73f3
bagoly <szgabbor@gmail.com>
2016-11-14 01:50:06 +0100

    Fizz write Fizz at every third numbers

--- new/Fizz.java
+++ new/Fizz.java
@@ -2,6 +2,11 @@ public class Fizz {
 
   public static void main(String[] args) {
     for (int i = 0; i < 100; i++) {
-      System.out.println(i);
+      if (i % 3 == 0) {
+        System.out.println("Fizz");
+      } else {
+        System.out.println(i);
+      }
     }
+  }
 }
